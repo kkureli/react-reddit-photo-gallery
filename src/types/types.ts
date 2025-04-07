bd7a896c7ae3c7be89e451e93d7a084359188c6f
Kaan Küreli <kkureli@gmail.com>
2025-04-07 19:55:39 +0300

    fix: type added for response

---
+++ src/types/types.ts
@@ -0,0 +1,28 @@
+// src/types/reddit.ts
+
+export type ImageData = {
+  id: string;
+  title: string;
+  subreddit: string;
+  author: string;
+  ups: number;
+  thumbnail: string;
+  url: string;
+  media_metadata: {
+    [key: string]: {
+      status: string;
+      e: string; // Image format (örneğin, "jpeg")
+      m: string; // MIME type (örneğin, "image/jpeg")
+      s: {
+        u: string; // Image URL
+        x: number; // Image width
+        y: number; // Image height
+      };
+    };
+  };
+  gallery_data?: {
+    items: {
+      media_id: string;
+    }[];
+  };
+};

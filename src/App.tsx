793c11e91e59f7ce3b4f46b0fcedbe5b791d14b0
Kaan Küreli <kkureli@gmail.com>
2025-04-07 19:55:10 +0300

    ui: stars added for bg

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage/HomePage";
 import PhotoDetail from "./pages/PhotoDetailPage/PhotoDetailPage";
 import Init from "./components/Init/Init";
+import Stars from "./components/Stars/Stars";
 import { store } from "./store/store";
 
 function App() {
   return (
     <Provider store={store}>
+      <Stars />
       <Init />
       <Routes>
         <Route path="/" element={<Home />} />

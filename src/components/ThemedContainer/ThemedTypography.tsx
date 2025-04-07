426934239adfef56ab7f367820626cf92a965806
Kaan Küreli <kkureli@gmail.com>
2025-04-07 22:34:13 +0300

    fix: text component updated for themes

--- src/components/ThemedContainer/ThemedTypography.tsx
+++ src/components/ThemedContainer/ThemedTypography.tsx
@@ -6,7 +6,7 @@ const ThemedTypography = (props: TypographyProps) => {
   return (
     <Typography
       {...props}
-      className={isDarkTheme ? "dark-theme" : "light-theme"}
+      className={isDarkTheme ? "dark-theme-text" : "light-theme-text"}
     />
   );
 };

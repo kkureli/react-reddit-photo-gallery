260bea92c9767af43869a2ac174dc0e9535c9e5b
Kaan Küreli <kkureli@gmail.com>
2025-04-08 14:16:41 +0300

    fix: locale init

--- src/components/Init/Init.tsx
+++ src/components/Init/Init.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { CACHE_KEYS } from "../../constants/variables";
 import { useDispatch } from "react-redux";
 import { setTheme } from "../../store/themeSlice";
 import { useTranslation } from "react-i18next";
 
-const Init = () => {
+const Init = ({ onInitDone }: { onInitDone: () => void }) => {
   const { i18n } = useTranslation();
   const dispatch = useDispatch();
+
+  const [languageInitialized, setLanguageInitialized] = useState(false);
 
   const setAppTheme = () => {
     const theme = localStorage.getItem(CACHE_KEYS.theme);
@@ -15,24 +17,28 @@ const Init = () => {
     }
   };
 
-  const setAppLanguage = () => {
+  const setAppLanguage = async () => {
     const language = localStorage.getItem(CACHE_KEYS.language);
-    if (language) {
-      i18n.changeLanguage(language);
+    if (language && language !== i18n.language) {
+      await i18n.changeLanguage(language);
     }
-  };
-
-  const initializeApp = () => {
-    setAppTheme();
-    setAppLanguage();
+    setLanguageInitialized(true);
   };
 
   useEffect(() => {
+    const initializeApp = async () => {
+      setAppTheme();
+      await setAppLanguage();
+      onInitDone();
+    };
     initializeApp();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <></>;
+  if (!languageInitialized) {
+    return null;
+  }
+
+  return null;
 };
 
 export default Init;
